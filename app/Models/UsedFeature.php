1a624ad58b13272d2a302e7b642842f57d78bb35
Edward Baitsewe <edward@monatemedia.com>
2024-08-24 18:32:37 +0200

    Prepare Models and Migrations

--- app/Models/UsedFeature.php
+++ app/Models/UsedFeature.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class UsedFeature extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'feature_id',
+        'user_id',
+        'credits',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function feature()
+    {
+        return $this->belongsTo(Feature::class);
+    }
 }
--- database/migrations/2024_08_24_154540_create_features_table.php
+++ database/migrations/2024_08_24_154540_create_features_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('route_name');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('required_credits');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
